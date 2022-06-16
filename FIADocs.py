5f0946b1cffd167447d1f51de275a458762fa764
xhico <francgf@gmail.com>
2022-06-16 19:38:00 +0100

    Update FIADocs.py

--- FIADocs.py
+++ FIADocs.py
@@ -226,7 +226,7 @@ def main():
 
 if __name__ == "__main__":
     print("----------------------------------------------------")
-    print(datetime.datetime.strftime(datetime.datetime.utcnow(), "%Y/%m/%d %H:%M UTC"))
+    print(str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")))
 
     # Set temp folder
     tmpFolder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmp")
